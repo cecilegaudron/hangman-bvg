32eacd15df6131c98337d67a292efcac71dbe6ff
Cécile <ce.gaudron@gmail.com>
2024-02-23 18:41:05 +0100

    Create view and url for line u1

--- hangman_bvg/urls.py
+++ hangman_bvg/urls.py
@@ -2,6 +2,6 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-        # URL for list with trackings
     path('', views.index, name="index"),
+    path('execute/', views.game_u1, name='game_u1'),
 ]
--- hangman_bvg/views.py
+++ hangman_bvg/views.py
@@ -1,5 +1,16 @@
 from django.shortcuts import render
+from django.http import JsonResponse
+
+from .hangman_u1 import newGame, window
 
 def index(request):
     """ Basic view for Home Page"""
     return render(request, 'hangman_bvg/index.html')
+
+def game_u1(request):
+    newGame()
+    window.mainloop()
+    
+    return render(request, 'game_u1')
+    #result = "Code Python exécuté avec succès!"
+    #return JsonResponse({'result': result})
