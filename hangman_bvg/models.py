10f6b30cbd312792f652d18e0089d5ded9a4a360
Cécile <ce.gaudron@gmail.com>
2024-03-08 18:25:37 +0100

    Create a model to process the user's input

--- hangman_bvg/models.py
+++ hangman_bvg/models.py
@@ -1,3 +1,19 @@
 from django.db import models
+import json
 
-# Create your models here.
+class Guess(models.Model):
+    userLetterChoice = models.CharField(
+        "Choisir une lettre :", 
+        default = '',
+        max_length=1,
+        null=False,
+        blank=False
+        )
+    userChoice = models.JSONField(default=list)
+    lives = models.IntegerField(default=6)
+    
+    def get_userChoice(self):
+        return json.loads(self.userChoice)
+    
+    def set_userChoice(self, userChoice):
+        self.userChoice = json.dumps(userChoice)
