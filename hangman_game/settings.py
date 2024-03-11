5e2db55859abbba46dc8b8322569056eda2d10d3
Cécile <ce.gaudron@gmail.com>
2024-03-11 15:13:04 +0100

    Fix statifcfiles issue with Heroku deployment

--- hangman_game/settings.py
+++ hangman_game/settings.py
@@ -28,7 +28,7 @@ TEMPLATES_DIR = os.path.join(BASE_DIR, 'templates')
 SECRET_KEY = os.environ.get('SECRET_KEY', '')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = [
     "hangman-bvg-701f79a0e67f.herokuapp.com", "localhost", "127.0.0.1"
@@ -57,6 +57,7 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
+    'whitenoise.middleware.WhiteNoiseMiddleware',
 ]
 
 ROOT_URLCONF = 'hangman_game.urls'
